f93bc12ceb24c377c1505d877d39a9113dfdc6c6
guoshenga <guoshenga@digitalchina.com>
2020-07-14 11:24:55 +0800

    A分支

--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -23,6 +23,7 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // 这是A分支
             services.AddControllersWithViews();
         }
 
