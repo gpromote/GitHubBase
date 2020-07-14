9bf7443dd9903386ec240faa5e604ee036834ae6
guoshenga <guoshenga@digitalchina.com>
2020-07-14 11:49:59 +0800

    这是master

--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -23,7 +23,7 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //ÕâÊÇA·ÖÖ§
+            //这是master
             services.AddControllersWithViews();
         }
 
